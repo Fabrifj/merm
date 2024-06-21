ebae9e75cab31c92e15ac2b89149e9a5ca52bb45
Fabrifj <fabrfj@gmail.com>
2024-06-21 15:27:32 -0400

    add Reset.sh

---
+++ reset.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo chown shipsenergy:shipsenergy /home/shipsenergy/public_html/upload/*
+sudo chown shipsenergy:shipsenergy /home/shipsenergy/public_html/upload/jp_graph/graphs/*
+sudo chown shipsenergy:shipsenergy /home/shipsenergy/public_html/erms/includes/*
+sudo chown shipsenergy:shipsenergy /home/shipsenergy/public_html/upload/jp_graph/graphs/src/*
+sudo chown shipsenergy:shipsenergy /home/shipsenergy/public_html/erms/includes/src/*
+sudo systemctl restart apache_exporter.service
+sudo systemctl restart httpd
+echo "Ready"
