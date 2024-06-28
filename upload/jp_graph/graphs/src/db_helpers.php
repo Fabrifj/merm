7580ed5c2217ea8746a7869b5785b9284e27ecd5
Fabrifj <fabrfj@gmail.com>
2024-06-28 09:42:37 -0400

    [FE] Fix db annual function 2

--- upload/jp_graph/graphs/src/db_helpers.php
+++ upload/jp_graph/graphs/src/db_helpers.php
@@ -178,7 +178,7 @@ function fetch_Annual($log, $loopname) {
                     MAX(max_cost_kw) AS max_cost_kw, 
                     MAX(max_off_cost_kw) AS max_off_cost_kw,
                     AVG(daily_cost_kwh) AS avg_daily_cost_kwh, 
-                    AVG(daily_total_kwh) AS avg_daily_total_kwh
+                    AVG(daily_total_kwh) AS avg_daily_total_kwh,
                     COUNT(*) AS days
 
                 FROM (
