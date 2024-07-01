06e354407ea33b451cf08c04855b6ee1a6c4b313
Fabrifj <fabrfj@gmail.com>
2024-06-30 23:53:29 -0400

    [FE] build graph mod 3,  debuging 7

--- erms/includes/init_mgr.php
+++ erms/includes/init_mgr.php
@@ -906,6 +906,10 @@ case ERMS_Modules::PerformanceTrending: //"mod8":
     
     
     $graph=mod3_graph_multi($ships_data,$VAL["date_value_start"],$VAL["date_value_end"]);
+    $graph["timezone"] = $timezone;
+    $graph["log_interval"] = 300000;
+    $graph["date_start"] = $dates[0];
+    $graph["date_end"] = $dates[count($dates) - 1];
     $graph["data"] = $shipsData;
       // // Debugging
     $formattedMessage = print_r($graph, true);
