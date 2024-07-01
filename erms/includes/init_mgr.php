841e463891dd36e89264285e3eba32535678b44f
Fabrifj <fabrfj@gmail.com>
2024-07-01 17:30:41 -0400

    [FE] Start mod 8, debuggin total, debugginf query10

--- erms/includes/init_mgr.php
+++ erms/includes/init_mgr.php
@@ -759,12 +759,12 @@ case ERMS_Modules::PerformanceTrending: //"mod8":
       }else{
         $Ship_available[] =  1 ;
       }
-      $formattedMessage = print_r($results["kWh_day"], true);
+      $formattedMessage = print_r($results["avg_cost"], true);
       $testLogger->logInfo($formattedMessage);
 
-      $ships_data[$ship_aquisuite]["kWh_day"][] = $results["kWh_day"];
-      $ships_data[$ship_aquisuite]["Peak_Demand"][] = $results["Peak_Demand"]*1;
-      $ships_data[$ship_aquisuite]["Grand_Total_Lay_Day"][] = $results["Grand_Total_Lay_Day"];
+      $ships_data[$ship_aquisuite]["kWh_day"][] = $results["avg_cost"];
+      $ships_data[$ship_aquisuite]["Peak_Demand"][] = $results["avg_kw"]*1;
+      $ships_data[$ship_aquisuite]["Grand_Total_Lay_Day"][] = $results["avg_cost"];
 
       $formattedMessage = print_r($ships_data[$ship_aquisuite]["kWh_day"], true);
       $testLogger->logInfo($formattedMessage);
