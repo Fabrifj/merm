9113b16b7b1a8a4cca79bb9452a3a250c30eacd9
Fabrifj <fabrfj@gmail.com>
2024-09-11 21:26:54 -0400

    [FE] Delete fields from menu

--- erms/includes/init.php
+++ erms/includes/init.php
@@ -775,7 +775,7 @@ if($ship_count==1){
       $shipsData = fetch_unitary_mod3_graph($testLogger, $loopname,$field1, $field2, $startDate, $endDate);
       $testLogger->logDebug("Fields1: " . $field1 . " Field2: ".$field2);
     } catch (Exception $e) {
-      $testLogger->logError("Error fetching EnergyMeters: " . $e->getMessage());
+      $testLogger->logError("Error gitfetching EnergyMeters: " . $e->getMessage());
     }
 
     $timezone = getTimezone($testLogger,$loopname);
