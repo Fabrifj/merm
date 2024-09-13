b110b31e6ef7dcfd9e5cb9baa87890efde0db9de
Fabrifj <fabrfj@gmail.com>
2024-09-13 09:03:29 -0400

    [BE] Modify power format 2

--- upload/class_objects/records.php
+++ upload/class_objects/records.php
@@ -250,8 +250,8 @@ class RecordFactory {
                 );
             case 'device248':
                 return new RecordsTypeStandard($timezone,$data['time'],$data['error'],$data['Accumulated_Real_Energy:_Net_Import__Export'],$loopName,
-                    $data['Total_Net_Instantaneous_Real_P_Power']* 100,$data['Total_Net_Instantaneous_Reactive_Q_Power'],$data['Total_Net_Instantaneous_Apparent_S_Power_vector_sum'],
-                    $data['Total_Power_Factor_Total_KW_/_Total_KVA'],$data['Current_Average_of_Active_Phases'],$data['Real_Power_Phase_A']* 100,
+                    $data['Total_Net_Instantaneous_Real_P_Power'],$data['Total_Net_Instantaneous_Reactive_Q_Power'],$data['Total_Net_Instantaneous_Apparent_S_Power_vector_sum'],
+                    $data['Total_Power_Factor_Total_KW_/_Total_KVA']* 100,$data['Current_Average_of_Active_Phases'],$data['Real_Power_Phase_A']* 100,
                     $data['Real_Power_Phase_B'],$data['Real_Power_Phase_C'],$data['Power_Factor_Phase_A'],
                     $data['Power_Factor_Phase_B']* 100,$data['Power_Factor_Phase_C']* 100,$data['Voltage_Phase_AB'],
                     $data['Voltage_Phase_BC'],$data['Voltage_Phase_AC'],$data['Voltage_Phase_AN'],
